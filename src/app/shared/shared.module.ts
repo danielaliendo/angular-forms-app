281449250299849ac6206070d3c490a5c6994059
Daniela Liendo <daniela@ualabee.com>
2023-05-28 18:10:35 +0200

    side menu component was declared and exported

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideMenuComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SideMenuComponent
   ]
 })
 
