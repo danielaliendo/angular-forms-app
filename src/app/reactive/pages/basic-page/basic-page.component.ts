87ccde30bee9d9d45a9ffdedc3c7026947dbe785
Daniela Liendo <daniela@ualabee.com>
2023-05-28 17:11:47 +0200

    basic page component was created

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reactive-basic-page',
+  templateUrl: './basic-page.component.html',
+  styles: [
+  ]
+})
+
+export class BasicPageComponent {
+
+}
