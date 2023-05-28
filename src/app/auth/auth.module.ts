77f2e59e0666ff762480fe6a2c89cfcf63899df0
Daniela Liendo <daniela@ualabee.com>
2023-05-28 17:12:21 +0200

    register page component was declared

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
