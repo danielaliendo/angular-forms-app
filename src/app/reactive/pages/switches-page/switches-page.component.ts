d47977af8e9114f530dd17fc71d5f753adc4f929
Daniela Liendo <daniela@ualabee.com>
2023-05-28 17:12:59 +0200

    switches page component was declared

---
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reactive-switches-page',
+  templateUrl: './switches-page.component.html',
+  styles: [
+  ]
+})
+
+export class SwitchesPageComponent {
+
+}
