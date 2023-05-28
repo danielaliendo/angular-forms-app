f362c273e5a175063737c8c3bfe1c9469420bcdc
Daniela Liendo <daniela@ualabee.com>
2023-05-28 17:12:33 +0200

    dynamic page component was declared

---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reactive-dynamic-page',
+  templateUrl: './dynamic-page.component.html',
+  styles: [
+  ]
+})
+
+export class DynamicPageComponent {
+
+}
