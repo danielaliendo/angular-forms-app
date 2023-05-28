ab5f28cfffa56dcd833a3ddc4508e31d735578fc
Daniela Liendo <daniela@ualabee.com>
2023-05-28 17:12:03 +0200

    register page component was created

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-register-page',
+  templateUrl: './register-page.component.html',
+  styles: [
+  ]
+})
+
+export class RegisterPageComponent {
+
+}
