7788f55eb430e0a2191c9a465fdbeffd4ba27aa8
Daniela Liendo <daniela@ualabee.com>
2023-05-28 18:10:22 +0200

    side menu component was created

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html',
+  styles: []
+})
+
+export class SideMenuComponent {
+
+}
