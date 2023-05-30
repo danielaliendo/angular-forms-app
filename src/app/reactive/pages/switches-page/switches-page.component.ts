00f58087e25791ff46e110afe9e6f325a126bb0c
Daniela Liendo <daniela@ualabee.com>
2023-05-30 21:16:12 +0200

    switches page component was created

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'reactive-switches-page',
@@ -7,6 +8,51 @@ import { Component } from '@angular/core';
   ]
 })
 
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit{
+
+  public myForm:FormGroup = this.fb.group({
+    genders: ['Male', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  })
+
+  public person = {
+    gender: 'Female',
+    wantNotifications: false,
+    termsAndConditions: true,
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person)
+  }
+
+  isInvalidField(field: string): boolean {
+    const control = this.myForm.controls[field]
+    if (!control.errors) return false
+    return control.errors && control.touched
+  }
+
+  onSave() {
+
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched()
+      return
+    }
+
+    const {
+      termsAndCondition,
+      ...newPerson
+    } = this.myForm.value
+
+    this.person = newPerson
+
+    // delete this.person.termsAndConditions
+
+  }
+
+  protected readonly onsubmit = onsubmit;
+
 
 }
