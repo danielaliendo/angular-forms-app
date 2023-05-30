888d799115ab5b44a4c11f8bc3e53d1dc99e2cd8
Daniela Liendo <daniela@ualabee.com>
2023-05-30 21:18:22 +0200

    form validator function were created

---
+++ src/app/shared/validators/form-validators.ts
@@ -0,0 +1,19 @@
+import {FormControl, ValidationErrors} from "@angular/forms";
+
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+export const canBeStrider = (control: FormControl) : ValidationErrors | null => {
+
+  const value = control.value.trim().toLowerCase()
+
+  if (value === 'strider') {
+    return {
+      noStrider: true
+    }
+  }
+
+  return null
+
+}
+
